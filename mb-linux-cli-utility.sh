258d0ff0404181eb51edaf8e777840b4c0713637
Yocum <chyocum@gsiccorp.net>
2019-07-23 16:07:39 -0400

    Add spinner function

--- mb-linux-cli-utility.sh
+++ mb-linux-cli-utility.sh
@@ -249,6 +249,23 @@ checks() {
   check_sed
 }
 
+# Function to display a spinner
+spinner()
+{
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while ps a | awk '{print $1}' | grep -q "${pid}"; do
+    temp="${spinstr#?}"
+    printf " [%c]  " "${spinstr}"
+    spinstr=${temp}${spinstr%"${temp}"}
+    sleep "${delay}"
+    printf "\b\b\b\b\b\b"
+  done
+  printf "    \b\b\b\b"
+}
+
 # Function to create temp directory to neatly store temp files
 create_dir() {
   mkdir -p "${tempDir}"
